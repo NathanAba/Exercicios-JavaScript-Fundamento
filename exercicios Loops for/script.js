68799d3e8843ef7ac9c192c025fb75229fb515f1
Nathan Antunes <nathan.estudos10@gmail.com>
2022-06-27 20:32:30 -0300

    Confirmar

--- exercicios Loops for/script.js
+++ exercicios Loops for/script.js
@@ -1,14 +1,17 @@
-let SpaceshipName = prompt("Nome da espaço nave")
-let SpaceshipCaracter = prompt("Qual letra deseja substituir?")
-let SpaceshipSubstitute = prompt("Trocar por qual letra?")
+let Spaceship = prompt("Nome da espaço nave")
 
-let newNameSpaceship = ""
+let letraParaSubstituir = prompt("Qual caracter deseja substituir?")
 
-for ( ; ;) {
-  if(SpaceshipCaracter == SpaceshipName){
-    newNameSpaceship += SpaceshipSubstitute
-  }else {
-    newNameSpaceship += SpaceshipName[i]
+let letraSubstituida = prompt("Trocar por qual caracter?")
+
+let newSpaceship = "" // variavel vazia para armazernar novo nome
+
+for(let pos = 0; pos < Spaceship.length; pos++){ // (let pos = 0) defini uma váriavel para começar a contar do caracter 0. (pos < Spaceship;lenght) se a pos for menor que a quantidade de caracter do "spaceship", adicionar + uma posição (pos++)
+  if(Spaceship[pos] == letraParaSubstituir){ //se a letra na posição selecionada for igual a letra para substituir
+    newSpaceship += letraSubstituida // adicionar a letra na variável NewSpaceship
+  } else { // senão, adicionar a letra que já esta na posição
+    newSpaceship += Spaceship[pos]
   }
 }
-alert("O novo nome da nave é " + newNameSpaceship)
+
+alert("O novo nome da nave é " + newSpaceship)
