f18567878a470d94571767918e3cd08d1d6c5099
Nathan Antunes <nathan.estudos10@gmail.com>
2022-07-05 21:31:30 -0300

    .

---
+++ exercicios funções/script.js
@@ -0,0 +1,9 @@
+let nomeNave = prompt("Qual o nome da nave?")
+
+
+let escolhaDeOption = prompt("Escolha uma das opções: \n1- Acelerar a nave em 5km/s \n2- Desacelerar em 5km/s \n3- Imprimir dadps de bordo \nSair do Programa")
+
+switch(escolhaDeOption){
+  case "1":
+
+}
